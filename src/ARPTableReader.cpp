1bb17c472cc4882a77149b0b1ac306be24d2a2e5
Carmine Paolino <carmine@paolino.me>
2015-02-25 20:01:32 +0100

    Hopefully fixed a bug

--- src/ARPTableReader.cpp
+++ src/ARPTableReader.cpp
@@ -7,7 +7,7 @@ ARPTableReader::ARPTableReader(std::string iface, std::string p) {
     std::ifstream arp_table(path.c_str());
     std::getline(arp_table, line); // ignore first line
     if (arp_table.is_open()) {
-        while (getline(arp_table, line)) {
+        while (std::getline(arp_table, line)) {
             std::cout << line << std::endl;
             std::stringstream ss(line);
             std::string ip, dummy, interface;
