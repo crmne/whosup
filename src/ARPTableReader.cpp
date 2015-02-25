9d5b6b615f5aced20484aa3472dbb1223b060c2e
Carmine Paolino <carmine@paolino.me>
2015-02-25 20:37:29 +0100

    Attempting to fix infinite loop bug in OpenWRT

--- src/ARPTableReader.cpp
+++ src/ARPTableReader.cpp
@@ -6,15 +6,19 @@ ARPTableReader::ARPTableReader(std::string iface, std::string p) {
     std::string line;
     std::ifstream arp_table(path.c_str());
     std::getline(arp_table, line); // ignore first line
-    if (arp_table.is_open()) {
-        while (std::getline(arp_table, line)) {
-            std::stringstream ss(line);
-            std::string ip, dummy, interface;
-            ss >> ip >> dummy >> dummy >> dummy >> dummy >> interface;
-            if (iface.compare(interface) == 0) {
-                ips.push_back(ip);
-            }
-        }
-        arp_table.close();
+    if (!arp_table.is_open()) {
+        std::cerr << "Error while opening file." << std::endl;
+        exit(1);
     }
+
+    while (std::getline(arp_table, line) && arp_table.good()) {
+        ;
+        std::stringstream ss(line);
+        std::string ip, dummy, interface;
+        ss >> ip >> dummy >> dummy >> dummy >> dummy >> interface;
+        if (iface.compare(interface) == 0) {
+            ips.push_back(ip);
+        }
+    }
+    arp_table.close();
 }
