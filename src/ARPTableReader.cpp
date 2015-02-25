86c8ff0940e4180fb78fa70998bd29c94f82a9a6
Carmine Paolino <carmine@paolino.me>
2015-02-25 20:24:17 +0100

    Revert "Debug file read"
    This reverts commit 840e625f9b2755bb1ce162ceb3f0ddf56b927884.
    
    Conflicts:
    	src/ARPTableReader.cpp

--- src/ARPTableReader.cpp
+++ src/ARPTableReader.cpp
@@ -8,11 +8,9 @@ ARPTableReader::ARPTableReader(std::string iface, std::string p) {
     std::getline(arp_table, line); // ignore first line
     if (arp_table.is_open()) {
         while (std::getline(arp_table, line)) {
-            std::cout << line << std::endl;
             std::stringstream ss(line);
             std::string ip, dummy, interface;
             ss >> ip >> dummy >> dummy >> dummy >> dummy >> interface;
-            std::cout << ip << " -> " << interface << std::endl;
             if (iface.compare(interface) == 0) {
                 ips.push_back(ip);
             }
